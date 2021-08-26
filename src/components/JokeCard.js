e55476d49e30b2664bc718025877259e63c0c161
Alex <apfeif.12@gmail.com>
2021-08-26 15:23:23 -0700

    visual overhaul and small bugfixes

--- src/components/JokeCard.js
+++ src/components/JokeCard.js
@@ -20,7 +20,7 @@ const JokeCard = (props) => {
 
   const handleLoading = () => {
     if (loading) {
-      return <div>---Loading your joke, hang on a sec---</div>;
+      return <StyledLoading>---Loading your joke, please hang on a sec---</StyledLoading>;
     }
   };
 
@@ -37,16 +37,17 @@ const JokeCard = (props) => {
   }
 
   return (
-    <div>
+    <Page>
       <div>
-        <StyledHeader>Joke Machine</StyledHeader>
+        <StyledHeader>Click-for-a-Joke</StyledHeader>
         <StyledButton onClick={handleClickSetup}>NEW JOKE</StyledButton>
-        <StyledDiv>{handleLoading()}</StyledDiv>
-        <StyledDiv>{setup}</StyledDiv>
+        <StyledLoading>{handleLoading()}</StyledLoading>
+        <StyledSetup>{setup}</StyledSetup>
         <StyledButton onClick={handleClickDelivery}>PUNCHLINE</StyledButton>
-        <StyledDiv>{handleReadyCheck()}</StyledDiv>
+        <StyledDelivery>{handleReadyCheck()}</StyledDelivery>
+        <StyledName>By Alex Pfeifer</StyledName>
       </div>
-    </div>
+    </Page>
   );
 };
 
@@ -65,16 +66,76 @@ export default connect(mapStateToProps, {
   fetchJokeReady,
 })(JokeCard);
 
+const Page = styled.div`
+background-color: #01303f;
+color: white;
+font-family: PressStart2P;
+height: 100vh;
+`
+
 const StyledHeader = styled.h1`
 color: white;
-text-decoration: underline;
 text-decoration: bold;
+font-size: 60px;
+font-family: PressStart2P;
+padding-top: 5%;
+padding-bottom: 5%;
+margin-top: 0;
 `
 
 const StyledButton = styled.button`
-color: white;
-background-color: black;
+background: #cf3a5a;
+background-image: -webkit-linear-gradient(top, #cf3a5a, #cf3a5a);
+background-image: -moz-linear-gradient(top, #cf3a5a, #cf3a5a);
+background-image: -ms-linear-gradient(top, #cf3a5a, #cf3a5a);
+background-image: -o-linear-gradient(top, #cf3a5a, #cf3a5a);
+background-image: linear-gradient(to bottom, #cf3a5a, #cf3a5a);
+-webkit-border-radius: 5;
+-moz-border-radius: 5;
+border-radius: 5px;
+font-family: PressStart2P;
+color: #ffffff;
+font-size: 30px;
+padding: 10px 20px 10px 20px;
+text-decoration: none;
+&:hover {
+  background: #cf3a5a;
+  background-image: -webkit-linear-gradient(top, #cf3a5a, #cc5a72);
+  background-image: -moz-linear-gradient(top, #cf3a5a, #cc5a72);
+  background-image: -ms-linear-gradient(top, #cf3a5a, #cc5a72);
+  background-image: -o-linear-gradient(top, #cf3a5a, #cc5a72);
+  background-image: linear-gradient(to bottom, #cf3a5a, #cc5a72);
+  text-decoration: none;
+}
 `
-const StyledDiv = styled.div`
-color: white
+
+const StyledLoading = styled.h2`
+color: white;
+font-size: 30px;
+`
+
+const StyledSetup = styled.h2`
+color: white;
+font-size: 30px;
+padding-left: 1%;
+padding-right: 1%;
+
+
+`
+
+const StyledDelivery = styled.h2`
+color: white;
+font-size: 20px;
+padding-left: 1%;
+padding-right: 1%;
+padding-top: 5%;
+
+`
+
+const StyledName = styled.h2`
+color: white;
+font-size: 14px;
+position: absolute;
+bottom: 0px;
+
 `
